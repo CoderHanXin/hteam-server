2a73f4decd9a1e4bc28f0c1afdd5b5b2323a87be
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-22 19:34:14 +0800

    add router

--- app/router.js
+++ app/router.js
@@ -6,9 +6,12 @@
 module.exports = app => {
   const { router, controller } = app
   router.get('/', controller.home.index)
+  router.get('/api/user', controller.user.index)
   router.post('/api/user/login', controller.user.login)
   router.post('/api/user/create', controller.user.create)
   router.del('/api/user/delete', controller.user.delete)
   router.put('/api/user/update', controller.user.update)
   router.get('/api/user/search', controller.user.search)
+  router.get('/api/org', controller.org.index)
+  router.get('/api/org/tree', controller.org.tree)
 }
