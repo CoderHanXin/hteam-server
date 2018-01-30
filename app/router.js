ed9156f5165d2436e61e7f85bc0dee88aa1e84fb
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-30 13:30:42 +0800

    change router

--- app/router.js
+++ app/router.js
@@ -8,10 +8,9 @@ module.exports = app => {
   router.get('/', controller.home.index)
   router.get('/api/user', controller.user.index)
   router.post('/api/user/login', controller.user.login)
-  router.post('/api/user/create', controller.user.create)
+  router.post('/api/user', controller.user.create)
   router.del('/api/user/delete', controller.user.delete)
   router.put('/api/user/update', controller.user.update)
-  router.get('/api/user/search', controller.user.search)
 
   router.get('/api/team', controller.team.index)
   router.get('/api/team/:id', controller.team.show)
