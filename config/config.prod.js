c4e27b3e80de29ff346df78c9bb85da272048396
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-01 18:52:52 +0800

    add sequelize config

--- config/config.prod.js
+++ config/config.prod.js
@@ -6,8 +6,17 @@ exports.mysql = {
     port: '3306',
     user: 'root',
     password: '123456',
-    database: 'hteam',
+    database: 'hteam'
   },
   app: true,
-  agent: false,
+  agent: false
+}
+
+exports.sequelize = {
+  dialect: 'mysql',
+  database: 'hteam',
+  host: '127.0.0.1',
+  port: '3306',
+  username: 'root',
+  password: '123456'
 }
