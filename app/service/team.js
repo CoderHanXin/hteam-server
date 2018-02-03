be1fd9da3ec59180def72d2f380e2159879bc917
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-03 14:28:24 +0800

    use sequelize model

--- app/service/team.js
+++ app/service/team.js
@@ -4,31 +4,25 @@ const Service = require('egg').Service
 
 class TeamService extends Service {
   async findById(id) {
-    const result = await this.app.mysql.get('t_team', { id })
+    const result = await this.app.model.Team.findById(id)
     return result
   }
 
   async findByUserId(userId) {
-    let _sql = 'select t.id, t.name from t_team t, '
-    _sql += ' (select teamId from r_team_user where userId = ?) r '
-    _sql += ' where t.id = r.teamId'
-    const result = await this.app.mysql.query(_sql, userId)
+    const user = await this.app.model.User.findById(userId)
+    const result = await user.getTeams()
     return result
   }
 
   async create(team) {
-    team.createTime = this.app.mysql.literals.now
-    const result = await this.app.mysql.insert('t_team', team)
-    return result
-  }
-
-  async delete(id) {
-    const result = await this.app.mysql.delete('t_team', { id })
+    const result = await this.app.model.Team.create(team)
     return result
   }
 
   async update(team) {
-    const result = await this.app.mysql.update('t_team', team)
+    const result = await this.app.model.Team.update(team, {
+      where: { id: team.id }
+    })
     return result
   }
 
