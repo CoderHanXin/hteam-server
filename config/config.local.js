8c3c101e429a4766086087460b44418449f68ade
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-12 11:11:33 +0800

    chore: add sequelize config

--- config/config.local.js
+++ config/config.local.js
@@ -11,3 +11,16 @@ exports.mysql = {
   app: true,
   agent: false,
 }
+
+exports.sequelize = {
+  dialect: 'mysql',
+  database: 'hteam_test',
+  host: '127.0.0.1',
+  port: '3306',
+  username: 'root',
+  password: '123456'
+}
+
+exports.security = {
+  domainWhiteList: [ 'http://localhost:9001' ],
+}
