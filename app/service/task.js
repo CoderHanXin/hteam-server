25f145368fcbcb5823e9ed837a02958b9b456f09
coderhanxin <coder.hanxin@gmail.com>
2018-02-13 18:30:35 +0800

    chore: delete unused code

--- app/service/task.js
+++ app/service/task.js
@@ -41,31 +41,6 @@ class TaskService extends Service {
     const result = await this.app.model.Task.update(task)
     return result
   }
-
-  // async findByAssignee(assignee, type) {
-  //   const values = []
-  //   let _sql = 'select * from task '
-  //   let _where = ' where assigneeId = ? '
-  //   values.push(assignee)
-  //   switch (Number(type)) {
-  //     case taskSearch.TASK_SEARCH_ALL:
-  //       break
-  //     case taskSearch.TASK_SEARCH_COMPLETE:
-  //       _where += ' and complete = ? '
-  //       values.push(taskSearch.TASK_SEARCH_COMPLETE)
-  //       break
-  //     case taskSearch.TASK_SEARCH_UNCOMPLETE:
-  //       _where += ' and complete = ? '
-  //       values.push(taskSearch.TASK_SEARCH_UNCOMPLETE)
-  //       break
-  //     default:
-  //       break
-  //   }
-
-  //   _sql += _where + ' order by id desc'
-  //   const result = await this.app.mysql.query(_sql, values)
-  //   return result
-  // }
 }
 
 module.exports = TaskService
