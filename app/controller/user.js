e09d61cf2654414cc4c9cb6b1f115de8d1ddc3bd
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-27 15:14:43 +0800

    chore: delete username from result

--- app/controller/user.js
+++ app/controller/user.js
@@ -31,6 +31,7 @@ class UserController extends Controller {
 
     if (result) {
       if (result.password === md5(password, this.config.md5Key)) {
+        delete result.username
         delete result.password
         this.success(result)
       } else {
